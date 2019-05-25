7eabe0e614f64d288f9631c287dc5499134f5f46
Ben Coleman <benc.uk@gmail.com>
2019-05-25 10:01:11 +0100

    Regex fix for slight change in PDF format

--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
-# Converter for M&S PDF bank statement to QIF file
+# M&S Bank Statement QIF Converter
+This is a *very* specific project!
+
+It reads Marks & Spencer's bank statements in PDF format and converts them to QIF to be imported into Microsft Money or other finance software. That's all it does 😄
+
+It's written in Node/JavaScript so will require the [Node.js runtime installed](https://nodejs.org/en/download/)
+
+### Usage
+```
+node run.js {filename.pdf}
+```
+If no filename is provided then **statement.pdf** will be used. The resulting QIF will be written to **output.qif**
--- run.js
+++ run.js
@@ -5,69 +5,77 @@ const pdf = require('pdf-parse');
 // Check param inputs
 let inputPdf = 'statement.pdf';
 if(process.argv[2]) {
-  console.log("Note! No input PDF specified, will use statement.pdf");
   inputPdf = process.argv[2];
+} else {
+  console.log("### No input PDF specified, will default to statement.pdf");
 }
 var transactions = { cash: [] };
 
-console.log(`Processing PDF '${inputPdf}'`);
+console.log(`### Processing PDF '${inputPdf}'`);
 
 // Load PDF and convert to text
 let pdfBuffer = fs.readFileSync(inputPdf);
 pdf(pdfBuffer)
 .then(pdf => {
   //console.log(pdf.text);
+
   // Horrific regex
-  let pdfRegex = /\d\d\s\D\w\w\s\d.*?£[\d,]+\.\d\d(\sCR)?/mgi;
+  let pdfRegex = /\d\d\s\w\w\w\s\d\d\s\d\d\s\w\w\w\s\d\d.*?£[\d,]+\.\d\d(\sCR)?/mgi;
+  
   // Run PDF text through regex
   let pdfMatches = pdf.text.match(pdfRegex)
   if(!pdfMatches || pdfMatches.length <= 0) {
-    console.log(`Regex match failure on whole text, this probably isn't an M&S bank statement!`);
+    console.log(`### Regex match failure on whole text, this probably isn't an M&S bank statement!`);
     process.exit(1);
   }
-  console.log(`Found ${pdfMatches.length} transactions in PDF statement`);
+  console.log(`### Found ${pdfMatches.length} transactions in PDF statement`);
   
   for(let transLine of pdfMatches) {
-    
-    // Another horrific regex
-    // Capture four groups, date1, date2, payee and amount
-    let transRegex = /(\d\d \w\w\w \d\d)\s(\d\d \w\w\w \d\d)\s(.*?)£(.*?)$/i
-    let transMatches = transLine.match(transRegex);
-    if(!transMatches || transMatches.length <= 0) {
-      console.log(`Regex match failure on transactions, this probably isn't an M&S bank statement!`);
-      process.exit(1);
-    }
+    //console.log(`${transLine}||||\n`);
+    try {
+      // Another horrific regex
+      // Capture four groups, date1, date2, payee and amount
+      let transRegex = /(\d\d \w\w\w \d\d)\s(\d\d \w\w\w \d\d)\s(.*?)£(.*?)$/i
+      let transMatches = transLine.match(transRegex);
+      if(!transMatches || transMatches.length <= 0) {
+        console.log(`### Regex match failure on transactions, this probably isn't an M&S bank statement!`);
+        console.log(`### Bad transaction line was\n ${transLine}\n<END OF LINE>`);
+        process.exit(1);
+      }
 
-    let amount = 0;
-    // Handle credits (with CR at the end of the amount)
-    if(transMatches[4].lastIndexOf('CR') >= 0) {
-      amount = transMatches[4].substr(0, transMatches[4].length-2);
-    } else {
-      amount = -transMatches[4];
+      let amount = 0;
+      // Handle credits (with CR at the end of the amount)
+      if(transMatches[4].lastIndexOf('CR') >= 0) {
+        amount = transMatches[4].substr(0, transMatches[4].length-2);
+      } else {
+        amount = -transMatches[4];
+      }
+      amount = (amount+"").replace(',', '');
+      
+      // Note match[0] is not any use, so captured groups start at 1
+      transactions.cash.push({
+        date: parseDate(transMatches[2]),    
+        payee: transMatches[3],   
+        amount: amount, 
+      });
+      console.log(`# ${parseDate(transMatches[2])}\tamount=${amount}\tpayee=${transMatches[3]}`);
+    } catch(e) {
+      console.log(`### ERROR ${e.toString()}`);
     }
-    amount = (amount+"").replace(',', '');
-    
-    // Note match[0] is not any use, so captured groups start at 1
-    transactions.cash.push({
-      date: parseDate(transMatches[2]),    
-      payee: transMatches[3],   
-      amount: amount, 
-    });
-    console.log(`##### ${parseDate(transMatches[2])}\t${amount}\t\t${transMatches[3]}`);
   }
 
   // Write output file
   qif.writeToFile(transactions, './output.qif', function (err, qifData) {
     if(err) {
-      console.log(`Error writing QIF`);
+      console.log(`### Error writing QIF`);
     } else {
-      console.log(`Written QIF file to output.qif`);
+      console.log(`### Written QIF file to output.qif`);
     }
   });
 })
 .catch(err => {
   console.log(`${err}`);
-  console.log(`Error converting PDF to text, maybe it's not a PDF? ${err}`);
+  console.log(`### Error converting PDF to text, maybe it's not a PDF? ${err}`);
 });
 
 function parseDate(s) {
